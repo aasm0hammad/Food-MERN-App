b99f9f8d23ef742ca50ef3ed0e9dfb97eba3f5a6
AASM0 <aas006900@gmail.com>
2024-09-08 00:06:19 +0530

    comment remove

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -8,7 +8,7 @@ router.route("/check-auth").get(isAuthenticated, checkAuth);
 router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/logout").post(logout);
-//router.route("/verify-email").post(verifyEmail);
+router.route("/verify-email").post(verifyEmail);
 router.route("/forgot-password").post(forgotPassword);
 router.route("/reset-password/:token").post(resetPassword);
 router.route("/profile/update").put(isAuthenticated,updateProfile);
